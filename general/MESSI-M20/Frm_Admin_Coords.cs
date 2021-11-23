4413c1e0c2f02d18a35ec6cfb6f2ef93d11eb439
DevPaski <71769140+DevPaski@users.noreply.github.com>
2021-11-23 20:12:17 +0100

    Funcional

--- general/MESSI-M20/Frm_Admin_Coords.cs
+++ general/MESSI-M20/Frm_Admin_Coords.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace MESSI_M20
 {
     public partial class Frm_Admin_Coords : Form
@@ -15,6 +17,8 @@ namespace MESSI_M20
         private bool verify_generate_button = false, imprimir = true;
         Dictionary<string, string> codes_coords = new Dictionary<string, string>();
         Font fnt = new Font("Dubai", 12);
+
+        //bool verify = true;
 
         public Frm_Admin_Coords()
         {
@@ -30,13 +34,79 @@ namespace MESSI_M20
             frm.ShowDialog();
         }
         #endregion
+        /*
+        // Connexió a Base de Dades
+        #region Connexió a Base de Dades
+
+        // Variables de connexió
+        #region Variables Connexió DB
+
+        SqlConnection cns;
+        SqlDataAdapter adapter;
+        DataSet dts;
+        string query;
+
+        #endregion
+
+        // Funcions Bàsiques
+        #region Funcions Bàsiques
+
+        // Funció de connexió
+        private void ConnectDB()
+        {
+            string connexio;
+            connexio = "Data Source=WHITEWOLF\\SQLEXPRESS;Initial Catalog=DarkCore;Integrated Security=True";
+
+            cns = new SqlConnection(connexio);
+        }
+
+        // Consulta a Base de Dades
+        private void QueryDB()
+        {
+            query = "select * from AdminCoordinates";
+            adapter = new SqlDataAdapter(query, cns);
+
+            cns.Open();
+            dts = new DataSet();
+            adapter.Fill(dts, "AdminCoordinates");
+            cns.Close();
+        }
+
+        // Funció per actualitzar la DB
+        private void UpdateDB()
+        {
+            int sol;
+
+            cns.Open();
+
+            SqlDataAdapter adapter;
+            adapter = new SqlDataAdapter(query, cns);
+
+            SqlCommandBuilder Cmd = new SqlCommandBuilder(adapter);
+
+            if (dts.HasChanges())
+            {
+                sol = adapter.Update(dts.Tables[0]);
+                MessageBox.Show("S'han modificat: " + sol.ToString() + " registres.");
+            }
+
+            cns.Close();
+            QueryDB();
+        }
+
+        #endregion
+
+        #endregion
+        */
 
         // Boto per a generar una taula
         #region Generar taula
         private void btn_generate_Click(object sender, EventArgs e)
         {
+            table_layout_pnl_coord.Hide();
+            table_layout_pnl_coord.Controls.Clear();
             codes_coords.Clear();
-            int limit = 20, count;
+            int limit = 20, count, rowCount;
 
             verify_generate_button = true;
             imprimir = true;
@@ -48,20 +118,39 @@ namespace MESSI_M20
 
             codes = codes_list.ToArray();
 
+            Label lbl_main = new Label();
+            lbl_main.Font = fnt;
+            lbl_main.Text = "";
+            lbl_main.TextAlign = ContentAlignment.MiddleCenter;
+            lbl_main.Dock = DockStyle.Fill;
+            lbl_main.Anchor = AnchorStyles.None;
+            table_layout_pnl_coord.Controls.Add(lbl_main, 0, 0);
+
+            rowCount = 1;
             count = 0;
             for (char i = 'A'; i < 'E'; i++)
             {
-                if (imprimir)
-                {
-                    Label lbl = new Label();
-                    lbl.Text = 
-                    table_layout_pnl_coord.RowStyles.Add(lbl);
-                }
+                Label lbl_char = new Label();
+                lbl_char.Font = fnt;
+                lbl_char.Text = i.ToString();
+                lbl_char.TextAlign = ContentAlignment.MiddleCenter;
+                lbl_char.Dock = DockStyle.Fill;
+                lbl_char.Anchor = AnchorStyles.None;
+                table_layout_pnl_coord.Controls.Add(lbl_char, 0, rowCount);
+
+                rowCount++;
+
                 for (int j = 1; j < 6; j++)
                 {
                     if (imprimir)
                     {
-
+                        Label lbl_num = new Label();
+                        lbl_num.Font = fnt;
+                        lbl_num.Text = j.ToString();
+                        lbl_num.TextAlign = ContentAlignment.MiddleCenter;
+                        lbl_num.Dock = DockStyle.Fill;
+                        lbl_num.Anchor = AnchorStyles.None;
+                        table_layout_pnl_coord.Controls.Add(lbl_num, j, 0);
                     }
                     if (count < limit)
                     {
@@ -70,8 +159,26 @@ namespace MESSI_M20
                     }
                 }
 
-                imprimir = true;
+                imprimir = false;
             }
+            /*
+            if (verify)
+            {
+                foreach (KeyValuePair<string, string> keyValue in codes_coords)
+                {
+                    DataRow DataR = dts.Tables[0].NewRow();
+                    DataR["DictKey"] = codes_coords.Keys;
+                    DataR["DictValue"] = codes_coords.Values;
+                    dts.Tables[0].Rows.Add(DataR);
+                }
+            }*/
+
+            //UpdateDB();
+            //QueryDB();
+
+            //verify = false;
+
+            
         }
 
         private HashSet<string> Generate_Codes(HashSet<string> codes_hash, ref int limit, string[] codes)
@@ -103,9 +210,7 @@ namespace MESSI_M20
         // Boto per a ensenyar la taula
         #region Botó Show
         private void btn_show_Click(object sender, EventArgs e)
-        {
-            table_layout_pnl_coord.Controls.Clear();
-
+        {    
             if (verify_generate_button == false)
             {
                 MessageBox.Show("Error, no s'han generat les coordenades.", "SITH CONTROLLER: ERROR 02");
